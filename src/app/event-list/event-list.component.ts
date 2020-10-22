1277bc7cd7242e44ddb8a55050d4ba92d9908b88
Nenad Vuchkovikj <nenad.vuchkovikj1@gmail.com>
2020-10-22 10:37:32 +0200

    add/delete clean

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -7,16 +7,44 @@ import { EventsServiceService } from '../service/events-service.service';
   styleUrls: ['./event-list.component.scss']
 })
 export class EventListComponent implements OnInit {
-  peopleGoing: number[];
   constructor(private eS: EventsServiceService) { }
 
   events: any[] = [];
   ngOnInit(): void {
       this.eS.getEventsFromDB().subscribe(ev => {
-        if(ev.length !== this.events.length){
-          this.events = ev;
+        if(ev.length > this.events.length){
+          ev.forEach(event => {
+            var bool:Boolean = false;
+            this.events.forEach(myEvent => {
+              if(event.id === myEvent.id){
+                bool = true;
+              }
+            })
+            if(!bool){
+              this.events.push(event);
+              this.events.sort(function compare(a,b ) {
+                if ( a.data.dateCreated < b.data.dateCreated ){
+                  return 1;
+                }
+                if ( a.data.dateCreated > b.data.dateCreated ){
+                  return -1;
+                }
+                return 0;
+              })
+            }
+          });
+        }else if(ev.length < this.events.length){
+              var ID;
+              this.events.forEach((myEvent,index) => {
+                  if(ev.some(ev => ev.id === myEvent.id)){
+                  } else{
+                    ID = index;
+                  }
+              });
+              this.events.splice(ID, 1);
         }
       });
+
   }
 
 }
